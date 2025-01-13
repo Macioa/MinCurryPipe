0d1a4c21baf110398dae0d03335f10087196c236
Ryan Montgomery <ryan.montgomery@frayt.com>
2025-01-13 13:44:06 -0700

    add curry and pipe

---
+++ src/index.ts
@@ -0,0 +1,73 @@
+interface PipeFn {
+  (...args: [any, ...Function[]]): any;
+}
+/* curried => Convert a function to a curried function.
+            Example: 
+                const StandardAdd = (a, b) => a + b;
+                const StandardAdd3 = (a, b, c) => a + b + c;
+                console.log(StandardAdd(1, 2)); // 3
+                const CurriedAdd = curried(StandardAdd);
+                console.log(CurriedAdd(1)(2)); // 3
+                const CurriedAdd3 = curried(StandardAdd3);
+                console.log(CurriedAdd3(1)(2,3)); // 6
+    */
+
+const curried = (fn: Function) => {
+  const curry = (...args: any[]) =>
+    args.length >= fn.length
+      ? fn(...args)
+      : (...nextArgs: any[]) => curry(...args, ...nextArgs);
+  return curry;
+};
+/* pipeArg => Pipe an object or value through a series of curried functions.
+            Example:
+                const StandardAdd = (a, b) => a + b;
+                const StandardAdd3 = (a, b, c) => a + b + c;
+
+                const CurriedAdd = (a) => (b) => a + b;
+                const AltCurriedAdd = curried(StandardAdd);
+                const CurriedAdd3 = curried(StandardAdd3);
+  
+                const result = pipeArg(
+                    1,
+                    CurriedAdd(2),
+                    AltCurriedAdd(3)
+                    CurriedAdd3(4, 5)
+                ); // 15
+  
+                const errorResult = pipe(1, StandardAdd(1)) // Error
+    */
+const pipeArg: PipeFn = (initialValue: any, ...fns: Function[]) =>
+  fns.reduce((acc, fn) => fn(acc), initialValue);
+
+/* pipeFns => Pipe a series of curried functions together.
+            Example:
+                const StandardAdd = (a, b) => a + b;
+
+                const CurriedAdd = (a) => (b) => a + b;
+                const AltCurriedAdd = curried(StandardAdd);
+  
+                const RunPipe = pipeFns(
+                    CurriedAdd(2),
+                    AltCurriedAdd(3)
+                ); 
+                const result = RunPipe(1); // 6
+  
+                const ErrorPipe = pipeFns(StandardAdd(1), StandardAdd(1)) 
+                const errorResult = ErrorPipe(1) // Error
+    */
+const pipeFns: PipeFn =
+  (...fns: Function[]) =>
+  (x: any) =>
+    fns.reduce((v, f) => f(v), x);
+
+/* pipe => Dynamically pipeFns or pipeArg */
+const pipe: PipeFn = (...args: [any, ...Function[]]) => {
+  const [firstArg, ...fns] = args;
+  return typeof firstArg === "function"
+    ? pipeFns(firstArg, ...fns)
+    : pipeArg(firstArg, ...fns);
+};
+
+export type { PipeFn };
+export { pipe, curried };
