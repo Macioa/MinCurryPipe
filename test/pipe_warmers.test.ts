f58c8d2c1053e9c7934a004f9fb196f987280090
Ryan Montgomery <ryan.montgomery@frayt.com>
2025-03-22 12:39:41 -0500

    add warmer tests

--- test/pipe_warmers.test.ts
+++ test/pipe_warmers.test.ts
@@ -6,53 +6,73 @@ describe("onStep", () => {
       CurriedAdd = curried(StandardAdd),
       cache = {};
 
-    const RunPipe = 
-    onStep(
+    const RunPipe = onStep(
       (result, target, index) => (cache[index] = { result, target })
-    ).pipe(
-        CurriedAdd(2, 3), 
-        CurriedAdd(4)(5), 
-        CurriedAdd(6, 7)
-    );
+    ).pipe(CurriedAdd(2, 3), CurriedAdd(4)(5), CurriedAdd(6, 7));
 
     RunPipe(1);
     expect(cache).toMatchObject({
-        '0': {
-          result: 6,
+      "0": {
+        result: 6,
         //   target: [Function: partial_StandardAdd] { arity: 3, args: 2 }
-        },
-        '1': {
-          result: 15,
+      },
+      "1": {
+        result: 15,
         //   target: [Function: partial_StandardAdd] { arity: 3, args: 2 }
-        },
-        '2': {
-          result: 28,
+      },
+      "2": {
+        result: 28,
         //   target: [Function: partial_StandardAdd] { arity: 3, args: 2 }
-        }
-      })
+      },
+    });
   });
 
-    it("allows multiple onSteps and persists through faultSafe", () => {
-      const r1:any[] = [],
-        r2:any[] = [],
-        r3:any[] = [];
+  it("calls a function in each step of the pipe when pipe args are expressed as arrays", () => {
+    const StandardAdd = (a, b, c) => a + b + c,
+      cache = {};
 
-        onStep(
-            (result, target, index) => r1.push(result),
-            (result, _t, _i) => r2.push(result)
-        )
-        .faultSafe()
-        .onStep((result, _t, _i) => r3.push(result))
-        .pipe(
-            1,
-            (x) => x + 3,
-            (x) => x + 5
-            );
+    const RunPipe = onStep(
+      (result, target, index) => (cache[index] = { result, target })
+    ).pipe([StandardAdd, 2, 3], [StandardAdd, 4, 5], [StandardAdd, 6, 7]);
 
-        expect(r1).toStrictEqual([1, 4, 9]);
-        expect(r2).toStrictEqual([1, 4, 9]);
-        expect(r3).toStrictEqual([1, 4, 9]);
+    RunPipe(1);
+    expect(cache).toMatchObject({
+      "0": {
+        result: 6,
+        //   target: [Function: partial_StandardAdd] { arity: 3, args: 2 }
+      },
+      "1": {
+        result: 15,
+        //   target: [Function: partial_StandardAdd] { arity: 3, args: 2 }
+      },
+      "2": {
+        result: 28,
+        //   target: [Function: partial_StandardAdd] { arity: 3, args: 2 }
+      },
     });
+  });
+
+  it("allows multiple onSteps and persists through faultSafe", () => {
+    const r1: any[] = [],
+      r2: any[] = [],
+      r3: any[] = [];
+
+    onStep(
+      (result, target, index) => r1.push(result),
+      (result, _t, _i) => r2.push(result)
+    )
+      .faultSafe()
+      .onStep((result, _t, _i) => r3.push(result))
+      .pipe(
+        1,
+        (x) => x + 3,
+        (x) => x + 5
+      );
+
+    expect(r1).toStrictEqual([1, 4, 9]);
+    expect(r2).toStrictEqual([1, 4, 9]);
+    expect(r3).toStrictEqual([1, 4, 9]);
+  });
 });
 
 describe("faultSafe", () => {
@@ -61,6 +81,6 @@ describe("faultSafe", () => {
       throw "Error!";
     });
 
-    expect(true).toBeTruthy()
+    expect(true).toBeTruthy();
   });
 });
